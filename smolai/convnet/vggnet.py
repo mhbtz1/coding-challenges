c3ee476168230a4c6cbd119142049e4390f9aece
Matthew Habtezgi <mhbtz@mit.edu>
2025-07-11 23:23:23 -0400

    loss blowup

--- smolai/convnet/vggnet.py
+++ smolai/convnet/vggnet.py
@@ -2,15 +2,21 @@ from tqdm import tqdm
 import torch
 import torch.nn as nn
 import datasets
+import numpy as np
 from torchvision.datasets import MNIST
 from torchvision.transforms import transforms
 import matplotlib.pyplot as plt
+from dataclasses import dataclass
 
+@dataclass
+class TrainConfiguration:
+    H = 28
+    W = 28
 
 class VGGNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.model = torch.nn.Sequential(
+        self.feature_sampler = torch.nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=3, kernel_size=2),
             nn.Conv2d(in_channels=3, out_channels=6, kernel_size=4),
             nn.MaxPool2d(kernel_size=2),
@@ -19,18 +25,31 @@ class VGGNet(nn.Module):
             nn.Conv2d(in_channels=12, out_channels=48, kernel_size=2),
             nn.MaxPool2d(kernel_size=2),
             nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(768, 224),
+            nn.Flatten()
+        )
+
+
+        '''
+        self.flatten = nn.Flatten()
+        inp = torch.zeros(1, 1, TrainConfiguration.H, TrainConfiguration.W)
+        out = self.flatten(self.feature_sampler(inp))
+        shp = out.size(1)
+        '''
+
+        self.classifier = torch.nn.Sequential(
+            nn.Linear(in_features=768, out_features=224),
             nn.ReLU(),
-            nn.Linear(224, 100),
-            nn.Linear(100, 20),
+            nn.Linear(in_features=224, out_features=100),
+            nn.Linear(in_features=100, out_features=20),
             nn.ReLU(),
-            nn.Linear(20, 10)
+            nn.Linear(in_features=20, out_features=10)
 
         )
 
     def forward(self, x):
-        logits = torch.nn.functional.softmax(self.model(x), dim=1)
+        rpr = self.feature_sampler(x)
+        logits = self.classifier(rpr)
+        logits = torch.nn.functional.softmax(logits, dim=1)
         return logits
     
 
@@ -43,7 +62,12 @@ def train(model: VGGNet, train_dataloader: torch.utils.data.Dataset, optimizer: 
         for i, (input, label) in enumerate(train_dataloader):
             optimizer.zero_grad()
             output = model(input)
-            fn_loss = loss(output, label)
+
+            #print(f"label: {label}")
+            label_tensor = torch.nn.functional.one_hot(label, num_classes=10)
+            #print(f"label_tensor: {label_tensor}")
+
+            fn_loss = loss(output.float(), label_tensor.float())
             avg_loss += fn_loss.item()
             if i > 0 and i % 100 == 0:
                 print("Avg. loss on epoch {} on step {}: {}".format(epoch, i, round(avg_loss / float(i), 3)))
@@ -73,7 +97,23 @@ if __name__ == "__main__":
 
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
     test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=True)
-    
-    train(model=model, loss=loss, optimizer=optimizer, train_dataloader=train_dataloader)
-    test(model=model, test_dataloader=test_dataloader)
+
+    input, label = train_dataset[0]
+    input1, label1 = train_dataset[1]
+
+    inp_tensor = torch.cat([torch.unsqueeze(input, dim=0), torch.unsqueeze(input1, dim=0)], dim=0)
+
+    print(inp_tensor.size())
+    x = model(inp_tensor)
+    print(x)
+
+    labels = torch.Tensor([label, label1]).long()
+    print(labels)
+    print(torch.nn.functional.one_hot(labels, num_classes=10))
+
+    fn_loss = loss(x, labels)
+    print(fn_loss)
+
+    #train(model=model, loss=loss, optimizer=optimizer, train_dataloader=train_dataloader)
+    #test(model=model, test_dataloader=test_dataloader)
 
